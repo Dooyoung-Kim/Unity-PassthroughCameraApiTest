17f0926af3e61eb261f254ba5618fbae1feab81e
Dooyoung <dooyoung.kim@kaist.ac.kr>
2025-05-20 17:39:44 +0900

    take a picture function added

--- Assets/PassthroughCameraDisplay.cs
+++ Assets/PassthroughCameraDisplay.cs
@@ -5,6 +5,10 @@ public class PassthroughCameraDisplay : MonoBehaviour
 {
     public WebCamTextureManager webcamManager;
     public Renderer quadRenderer;
+    public string textureName;
+
+    private Texture2D picture;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +20,31 @@ public class PassthroughCameraDisplay : MonoBehaviour
     {
         if(webcamManager.WebCamTexture != null)
         {
-            quadRenderer.material.mainTexture = webcamManager.WebCamTexture;
+            if(OVRInput.GetDown(OVRInput.Button.One))
+            {
+                TakePicture();
+            }
         }
         
+    }
+
+    public void TakePicture()
+    {
+        int width = webcamManager.WebCamTexture.width;
+        int height = webcamManager.WebCamTexture.height;
+
+        if(picture == null)
+        {
+            picture = new Texture2D(width, height);
+        }
+
+        Color32[] pixels = new Color32[width * height];
+        webcamManager.WebCamTexture.GetPixels32(pixels);
+
+        picture.SetPixels32(pixels);
+        picture.Apply();
+
+        quadRenderer.material.SetTexture(textureName, picture);
+
     }
 }
